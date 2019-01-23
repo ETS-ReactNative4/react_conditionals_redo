30525289bbd8092704670bebb3a1ee7d52b09596
cl <christopherlu.pdx@gmail.com>
2019-01-23 14:42:59 -0800

    outsourced persons content from the return method

--- src/App.js
+++ src/App.js
@@ -49,15 +49,10 @@ class App extends Component {
       cursor: 'pointer'
     };
 
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Switch Name</button>
+    let persons = null;
 
-        {this.state.showPersons === true ?
+    if (this.state.showPersons) {
+      persons = (
       <div>
         <Person
           name={this.state.persons[0].name}
@@ -70,8 +65,18 @@ class App extends Component {
         <Person
           name={this.state.persons[2].name}
           age={this.state.persons[2].age} />
-        </div> : null
-        }
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name</button>
+          {persons}
     </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
